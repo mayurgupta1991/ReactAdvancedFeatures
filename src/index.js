d5c9d296dcb1f0f71ff03927d99bcd0abd9cd075
Mayur Gupta <mayur.gupta@tothenew.com>
2017-12-08 23:34:36 +0530

    Updated app for using provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import reducers from './reducers';
 import configureStore from './configureStore';
@@ -7,5 +8,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = configureStore(reducers);
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 registerServiceWorker();
